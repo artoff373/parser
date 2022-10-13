9ab197ccc1053a731f07b062a151a40def2f9a6f
Petr <artoff373@yandex.ru>
2022-10-13 11:50:12 +0300

    add checkbox handling on check

--- server/templates/js/script.js
+++ server/templates/js/script.js
@@ -1,1 +1,13 @@
-console.log('asdfasdf');
+const handleChange = (event)=> {
+  const parent = event.target.closest('.card');
+  const text = parent.querySelector('.js-text');
+  console.log(text);
+  const link = parent.querySelector('.js-title');
+  link.classList.add('collapsed');
+  text.classList.remove('collapsing');
+  setTimeout(()=>{
+      text.classList.remove('show');
+  },300)
+  
+  console.log(link);
+}
